0d1067b45dd77f822f61b1be789b13a94aac352b
Anton Rolin <antonrolin@hotmail.com>
2021-05-17 16:22:13 +0200

    Added MyMathCalc

---
+++ src/main/java/MyMathCalc.java
@@ -0,0 +1,21 @@
+/**
+ * Created by: Anton Rolin
+ * Date: 17/05/2021
+ * Time: 15:15
+ * Project: springdocker
+ * Copyright: MIT
+ */
+public class MyMathCalc {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int divide(int a, int b){
+        return a/b;
+    }
+}
